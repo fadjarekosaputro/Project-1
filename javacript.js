f78be18a3ef2adfd63d0fcf3f4ec95a93fb5114a
fadjarekosaputro <fadjaresaputra44@gmail.com>
2023-02-16 12:31:39 +0700

    upload project 1

---
+++ javacript.js
@@ -0,0 +1,28 @@
+const firstName = prompt("Masukan Nama anda");
+
+const user = {
+   name: {
+       first: firstName,
+   },
+};
+alert("Terimakasih " + user.name.first + "  telah mengunjungi website kami!");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
